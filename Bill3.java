eaa063bb56664c49a47f72c267a3f5ea97d5b6c1
zakfeingold <zakfeingold@hotmail.co.uk>
2025-11-06 15:29:57 +0200

    Completed Assignment

--- Bill3.java
+++ Bill3.java
@@ -10,7 +10,7 @@ public class Bill3 {
         int total = Integer.parseInt(args[3]);
         double bill = (total / 3.0);
         double per = Math.ceil(bill);
-        System.out.println("Dear " + name3 + ", " + name2 + ", " + name1 + ": pay " + per + " Shekels each");
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + per + " Shekels each");
 
 
 
